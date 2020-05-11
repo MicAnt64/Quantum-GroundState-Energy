f6ac266d9dd579af188c69726f7a1dae2e432272
Michael Antia <michael_antia@hotmail.com>
2020-05-11 11:37:12 -0400

    Load data

--- quantum_energy_net.py
+++ quantum_energy_net.py
@@ -13,3 +13,14 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import time
+
+main_folder = "/Users/michaelantia/Desktop/Quantum-GroundState-Energy/Data/"
+file_name   = "roboBohr.csv"
+df = pd.read_csv(main_folder+file_name)
+df = df.drop(['pubchem_id'], axis=1)
+df = df.drop(['Unnamed: 0'], axis=1)
+y = df['Eat']
+X = df.drop(['Eat'], axis=1)
+X_data = X.as_matrix()
+y_data = y.as_matrix()
+
